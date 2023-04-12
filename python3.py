09389e8079c4b927185bb2d2ea20c3907dcf5933
suranyak <31271068+suranyak@users.noreply.github.com>
2023-04-12 18:25:33 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,16 @@
+
+names = ['keer' ,'suru' ,'soo' ,44,56 ]
+print(names[0])
+list_1 = ['keer' ,'suru' ,'soo' ,44,56 ]
+list_2 = [1,2,3,4,5]
+list_1.append('hi')
+print(list_1)
+numbers = [1,34,23,76]
+numbers.sort()
+print(numbers)
+numbers.remove(34)
+print(numbers)
+print(sorted(numbers))
+
+#numbers1 = [3,'7',4,1'6',2,9]
+#numbers2 = (1,3,5,7,3)
